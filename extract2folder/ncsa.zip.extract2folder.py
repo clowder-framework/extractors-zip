737593e0d52107356103287426c4fa3bb724acca
Max Burnette <mburnet2@illinois.com>
2018-10-24 09:50:39 -0500

    flesh out extract2folder

--- extract2folder/ncsa.zip.extract2folder.py
+++ extract2folder/ncsa.zip.extract2folder.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import logging
+import zipfile
 
 from pyclowder.extractors import Extractor
 from pyclowder.utils import CheckMessage
+from pyclowder.files import upload_to_dataset
 
 
 class ZipContentsExtractor(Extractor):
@@ -25,7 +27,22 @@ class ZipContentsExtractor(Extractor):
         return CheckMessage.bypass
 
     def process_message(self, connector, host, secret_key, resource, parameters):
-        pass
+        zip = resource['local_paths'][0]
+        zipname = resource['name'].replace(".zip", "")
+
+        # Create folder with name of zipfile
+        url = '%sapi/datasets/%s/newFolder?key=%s' % (host, resource["parent"]["id"], secret_key)
+        folderid = connector.post(url, data={"name": zipname}, verify=connector.ssl_verify if connector else True)
+
+        # Extract files into new folder
+        zf = zipfile.ZipFile(zip)
+        contents = zf.namelist()
+        for filename in contents:
+            zf.extract(filename)
+            if not filename.endswith("/"):
+                fileid = upload_to_dataset(connector, host, secret_key, resource["parent"]["id"], filename)
+                url = '%sapi/datasets/%s/movefolder/%s/%s?key=%s' % (host, resource["parent"]["id"], folderid, fileid, secret_key)
+                connector.post(url)
 
 if __name__ == "__main__":
     extractor = ZipContentsExtractor()
